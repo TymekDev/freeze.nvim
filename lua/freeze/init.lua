2a324992dd9dca72ca5e898b458bef6e99737ba9
Tymoteusz Makowski <tymek.makowski@gmail.com>
2024-07-11 10:32:46 +0200

    feat: add initial implementation

---
+++ lua/freeze/init.lua
@@ -0,0 +1,92 @@
+local M = {}
+
+---@alias freeze.Lines number|[number, number]
+
+---@class freeze.Options
+---@field config? "base"|"full"|"user"|string
+---@field font.family? string
+---@field lines? freeze.Lines
+---@field output? string|fun(): string
+---@field theme? freeze.Theme|fun(): freeze.Theme
+local defaults = {
+  config = "full",
+  ["font.family"] = "JetBrainsMono Nerd Font",
+  output = function()
+    return vim.fn.tempname() .. ".png"
+  end,
+  theme = require("freeze.themes").tokyonight_storm,
+}
+
+---@param lines freeze.Lines
+---@return string # A value accepted by --lines flag
+local lines_to_value = function(lines)
+  if type(lines) == "number" then
+    lines = { lines, lines }
+  end
+  return table.concat(lines, ",")
+end
+
+---@param flags table<string, string>
+---@return string[]
+local freeze_cmd = function(flags)
+  local cmd = { "freeze" }
+  for opt, value in pairs(flags) do
+    table.insert(cmd, "--" .. opt)
+    table.insert(cmd, value)
+  end
+  table.insert(cmd, vim.fn.expand("%:p"))
+  return cmd
+end
+
+---@param path string
+---@return string[]
+local copy_cmd = function(path)
+  return {
+    "osascript",
+    "-e",
+    string.format('set the clipboard to (read (POSIX file "%s") as  {«class PNGf»})', path),
+  }
+end
+
+---@param cmd string[]
+---@param on_success? fun()
+local run_cmd = function(cmd, on_success)
+  vim.system(cmd, {}, function(result)
+    if result.code ~= 0 then
+      error(string.format("Failed to run %s: exit status %d", cmd[1], result.code))
+    end
+    if type(on_success) == "function" then
+      on_success()
+    end
+  end)
+end
+
+---When `opts.lines` are not provided, `freeze` is run on an entire file.
+---@param opts? freeze.Options
+M.generate = function(opts)
+  if vim.opt_local.modified:get() then
+    vim.notify("The current buffer has unsaved changes. Save and try again.", vim.log.levels.ERROR)
+    return
+  end
+
+  local flags = vim.tbl_deep_extend("force", defaults, opts or {})
+  flags.lines = lines_to_value(flags.lines or { 1, vim.api.nvim_buf_line_count(0) }) ---@diagnostic disable-line: assign-type-mismatch
+  if type(flags.output) == "function" then
+    flags.output = flags.output()
+  end
+  if type(flags.theme) == "function" then
+    flags.theme = flags.theme()
+  end
+
+  ---@cast flags table<string, string>
+  if string.match(flags.output, "%.png$") == nil then
+    vim.notify("Appended a missing .png extension to the output path", vim.log.levels.WARN)
+    flags.output = flags.output .. ".png"
+  end
+
+  run_cmd(freeze_cmd(flags), function()
+    run_cmd(copy_cmd(flags.output))
+  end)
+end
+
+return M
